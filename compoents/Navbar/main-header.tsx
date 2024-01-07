835b1bf4579c8db181c6640ceb6492e649b23bff
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-01-07 13:56:36 +0530

    nav bar done

--- compoents/Navbar/main-header.tsx
+++ compoents/Navbar/main-header.tsx
@@ -1,10 +1,28 @@
-"use client";
-import React, { useState } from "react";
-import classes from "./MainHeader.module.css";
+import React from "react";
 import Link from "next/link";
+import classes from "./mainheader.module.css";
 
 const MainHeader = () => {
-  return <></>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.c1}>
+        <Link href="/" className={classes.heading}>
+          uDecide
+        </Link>
+      </div>
+      <div className={classes.c2}>
+        <Link href="/newsletter" className={classes.item}>
+          Newsletter
+        </Link>
+        <Link href="/" className={`${classes.item} ${classes.btn}`}>
+          Sign In
+        </Link>
+        <Link href="/" className={`${classes.item} ${classes.btn}`}>
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default MainHeader;
